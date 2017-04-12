da85b541e68bf98794106d05623fb5bbd9a3e162
Scott <spelo3@gmail.com>
2017-04-12 16:05:30 -0700

    Initial Commit

---
+++ ScreenSwap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenSwap
+//
+//  Created by Scott Pelo on 4/12/17.
+//  Copyright © 2017 Scott Pelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
